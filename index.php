8449b4b6db0d62ea0401d4e37a7483065ebd2660
Jean Paul <jeanpaul@actionphp.com>
2013-07-24 12:47:15 +0100

    "Started integration into Wordpress"

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
+/*
+Plugin Name: Wordpress Shopping Cart
+Plugin URI: http://WPCart.net/shopping-cart
+Description: Simple Wordpress Shopping Cart 
+Version: 0.9
+Author: Jean Paul
+Author URI: http://ActionPHP.com
+License: GPL2
+*/
+?><?php
 chdir(dirname(__DIR__) . '/wpcart');
 
 require_once 'core/lib/Application/Application.php';
 $config = include('application/config/application.config.php');
 
 
-$route = isset($_GET['route']) ? $_GET['route'] : 'index';
-$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+$route = isset($_GET['wpcart_route']) ? $_GET['wpcart_route'] : 'index';
+$action = isset($_GET['wpcart_action']) ? $_GET['wpcart_action'] : 'index';
 $params = isset($_POST) ? $_POST : array();
 
 $application = new Application;
