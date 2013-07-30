941bb4ce81e14912f34abce6e52e65bc80a2ad2d
Jean Paul <jeanpaul@actionphp.com>
2013-07-30 14:17:10 +0100

    Added basic product table

--- init.php
+++ init.php
@@ -52,3 +52,32 @@ function wpcart_main_menu_products(){
 }
 
 //Let's create our database
+register_activation_hook(__FILE__, 'wpcart_product_table');
+function wpcart_product_table()
+	{
+		global $wpdb;
+		$table_name = $this->table->getTableName();
+
+		$sql = "
+
+		CREATE TABLE IF NOT EXISTS $table_name
+		(
+			id INT NOT NULL AUTO_INCREMENT
+			,PRIMARY KEY (id)
+			,family_id INT NOT NULL 
+			,category_id INT NOT NULL 
+			,sku VARCHAR(250) NOT NULL 
+			,name VARCHAR(255) NOT NULL 
+			,brief_description VARCHAR(250) NOT NULL 
+			,description TEXT NOT NULL 
+			,price DECIMAL(11, 2) NOT NULL 
+			,tags TEXT NOT NULL 
+		)
+
+		";
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	
+		dbDelta($sql);
+
+	}
